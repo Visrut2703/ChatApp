73ac22253bb174716170d8de23a3df9f68951fb4
Visrut2703 <100529711+Visrut2703@users.noreply.github.com>
2024-06-18 20:58:37 +0530

    Production

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from 'path'
+import path from "path";
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
-
-import connectToMongoDB from "./db/connectToMongoDB.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 
@@ -19,18 +18,19 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth" , authRoutes);
-app.use("/api/message" , messageRoutes);
-app.use("/api/users" , userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*")
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/" , (req , res)=>{
 //     res.send("Hello");
 // })
 
-server.listen(PORT , ()=>{
-    connectToMongoDB();
-    console.log(`Server listening at port ${PORT}`)
-    
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server listening at port ${PORT}`);
 });
